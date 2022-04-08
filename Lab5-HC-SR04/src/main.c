ff2b517a5be8ca10df7a4c2abba398c63ef94511
vigrel <eller.vini@gmail.com>
2022-04-08 15:58:15 -0300

    style(main): Microchip style pattern

--- Lab5-HC-SR04/src/main.c
+++ Lab5-HC-SR04/src/main.c
@@ -36,90 +36,90 @@ void but1_callback() {
 }
 
 void echo_callback() {
-    if (!pio_get(ECHO_PIO, PIO_INPUT, ECHO_IDX_MASK)) {
-        flag_echo_fall = 1;
-        flag_echo_rise = 0;
-    } else if (pio_get(ECHO_PIO, PIO_INPUT, ECHO_IDX_MASK)) {
-        flag_echo_rise = 1;
-        flag_echo_fall = 0;
-    }
+	if (!pio_get(ECHO_PIO, PIO_INPUT, ECHO_IDX_MASK)) {
+		flag_echo_fall = 1;
+		flag_echo_rise = 0;
+		} else if (pio_get(ECHO_PIO, PIO_INPUT, ECHO_IDX_MASK)) {
+		flag_echo_rise = 1;
+		flag_echo_fall = 0;
+	}
 }
 
 void trig_pulse() {
-    pio_set(TRIG_PIO, TRIG_IDX_MASK);
-    delay_us(10);
-    pio_clear(TRIG_PIO, TRIG_IDX_MASK);
+	pio_set(TRIG_PIO, TRIG_IDX_MASK);
+	delay_us(10);
+	pio_clear(TRIG_PIO, TRIG_IDX_MASK);
 }
 
 void init(void) {
-    pmc_enable_periph_clk(ECHO_PIO_ID);
-    pio_configure(ECHO_PIO, PIO_INPUT, ECHO_IDX_MASK, PIO_DEFAULT);
-    pio_handler_set(ECHO_PIO_ID,
-                    ECHO_PIO_ID,
-                    ECHO_IDX_MASK,
-                    PIO_IT_EDGE,
-                    echo_callback);
-    pio_enable_interrupt(ECHO_PIO, ECHO_IDX_MASK);
-    pio_get_interrupt_status(ECHO_PIO);
-    NVIC_EnableIRQ(ECHO_PIO_ID);
-    NVIC_SetPriority(ECHO_PIO_ID, 4);
+	pmc_enable_periph_clk(ECHO_PIO_ID);
+	pio_configure(ECHO_PIO, PIO_INPUT, ECHO_IDX_MASK, PIO_DEFAULT);
+	pio_handler_set(ECHO_PIO_ID,
+	ECHO_PIO_ID,
+	ECHO_IDX_MASK,
+	PIO_IT_EDGE,
+	echo_callback);
+	pio_enable_interrupt(ECHO_PIO, ECHO_IDX_MASK);
+	pio_get_interrupt_status(ECHO_PIO);
+	NVIC_EnableIRQ(ECHO_PIO_ID);
+	NVIC_SetPriority(ECHO_PIO_ID, 4);
 
-    pmc_enable_periph_clk(BUT1_PIO_ID);
-    pio_configure(BUT1_PIO, PIO_INPUT, BUT1_IDX_MASK, PIO_PULLUP | PIO_DEBOUNCE);
-    pio_handler_set(BUT1_PIO, BUT1_PIO_ID, BUT1_IDX_MASK, PIO_IT_EDGE,
+	pmc_enable_periph_clk(BUT1_PIO_ID);
+	pio_configure(BUT1_PIO, PIO_INPUT, BUT1_IDX_MASK, PIO_PULLUP | PIO_DEBOUNCE);
+	pio_handler_set(BUT1_PIO, BUT1_PIO_ID, BUT1_IDX_MASK, PIO_IT_EDGE,
 	but1_callback);
-    pio_enable_interrupt(BUT1_PIO, BUT1_IDX_MASK);
-    pio_get_interrupt_status(BUT1_PIO);
-    NVIC_EnableIRQ(BUT1_PIO_ID);
+	pio_enable_interrupt(BUT1_PIO, BUT1_IDX_MASK);
+	pio_get_interrupt_status(BUT1_PIO);
+	NVIC_EnableIRQ(BUT1_PIO_ID);
 	NVIC_SetPriority(BUT1_PIO_ID, 4);
 
-    pmc_enable_periph_clk(TRIG_PIO_ID);
-    pio_configure(TRIG_PIO, PIO_OUTPUT_1, TRIG_IDX_MASK, PIO_DEFAULT);
+	pmc_enable_periph_clk(TRIG_PIO_ID);
+	pio_configure(TRIG_PIO, PIO_OUTPUT_1, TRIG_IDX_MASK, PIO_DEFAULT);
 }
 
 void RTT_Handler(void) {
-    uint32_t ul_status;
-    ul_status = rtt_get_status(RTT);
-    if ((ul_status & RTT_SR_ALMS) == RTT_SR_ALMS) {
-    }
-    if ((ul_status & RTT_SR_RTTINC) == RTT_SR_RTTINC) {
-    }
+	uint32_t ul_status;
+	ul_status = rtt_get_status(RTT);
+	if ((ul_status & RTT_SR_ALMS) == RTT_SR_ALMS) {
+	}
+	if ((ul_status & RTT_SR_RTTINC) == RTT_SR_RTTINC) {
+	}
 }
 
 static void RTT_init(float freqPrescale, uint32_t IrqNPulses, uint32_t rttIRQSource) {
-    uint16_t pllPreScale = (int)(((float)32768) / freqPrescale);
-    rtt_sel_source(RTT, false);
-    rtt_init(RTT, pllPreScale);
-    if (rttIRQSource & RTT_MR_ALMIEN) {
-        uint32_t ul_previous_time;
-        ul_previous_time = rtt_read_timer_value(RTT);
-        while (ul_previous_time == rtt_read_timer_value(RTT));
-        rtt_write_alarm_time(RTT, IrqNPulses + ul_previous_time);
-    }
-    NVIC_DisableIRQ(RTT_IRQn);
-    NVIC_ClearPendingIRQ(RTT_IRQn);
-    NVIC_SetPriority(RTT_IRQn, 4);
-    NVIC_EnableIRQ(RTT_IRQn);
-    if (rttIRQSource & (RTT_MR_RTTINCIEN | RTT_MR_ALMIEN))
-        rtt_enable_interrupt(RTT, rttIRQSource);
-    else
-        rtt_disable_interrupt(RTT, RTT_MR_RTTINCIEN | RTT_MR_ALMIEN);
+	uint16_t pllPreScale = (int)(((float)32768) / freqPrescale);
+	rtt_sel_source(RTT, false);
+	rtt_init(RTT, pllPreScale);
+	if (rttIRQSource & RTT_MR_ALMIEN) {
+		uint32_t ul_previous_time;
+		ul_previous_time = rtt_read_timer_value(RTT);
+		while (ul_previous_time == rtt_read_timer_value(RTT));
+		rtt_write_alarm_time(RTT, IrqNPulses + ul_previous_time);
+	}
+	NVIC_DisableIRQ(RTT_IRQn);
+	NVIC_ClearPendingIRQ(RTT_IRQn);
+	NVIC_SetPriority(RTT_IRQn, 4);
+	NVIC_EnableIRQ(RTT_IRQn);
+	if (rttIRQSource & (RTT_MR_RTTINCIEN | RTT_MR_ALMIEN))
+	rtt_enable_interrupt(RTT, rttIRQSource);
+	else
+	rtt_disable_interrupt(RTT, RTT_MR_RTTINCIEN | RTT_MR_ALMIEN);
 }
 
 void TC0_Handler(void) {
 	volatile uint32_t status = tc_get_status(TC0, 0);
-    flag_trig = 1;
-    flag_sensor_problems++;
+	flag_trig = 1;
+	flag_sensor_problems++;
 
 }
 
 void TC3_Handler(void) {
 	volatile uint32_t status = tc_get_status(TC1, 0);
 	flag_idx_measures++;
-    if (flag_idx_measures == 5){
-        flag_idx_measures = 0;
-    }
-    flag_add_measure = 1;
+	if (flag_idx_measures == 5){
+		flag_idx_measures = 0;
+	}
+	flag_add_measure = 1;
 }
 
 void TC_init(Tc *TC, int ID_TC, int TC_CHANNEL, int freq) {
@@ -140,61 +140,61 @@ void TC_init(Tc *TC, int ID_TC, int TC_CHANNEL, int freq) {
 
 
 int main(void) {
-    char str[128];
+	char str[128];
 	double measures[5];
 	
-    WDT->WDT_MR = WDT_MR_WDDIS;
+	WDT->WDT_MR = WDT_MR_WDDIS;
 
-    board_init();
-    sysclk_init();
-    delay_init();
-    gfx_mono_ssd1306_init();
-    init();
-    
+	board_init();
+	sysclk_init();
+	delay_init();
+	gfx_mono_ssd1306_init();
+	init();
+	
 	TC_init(TC0, ID_TC0, 0,(int) 1/0.011764);
 	tc_start(TC0, 0);
 
-    TC_init(TC1, ID_TC3, 0, 1);
+	TC_init(TC1, ID_TC3, 0, 1);
 	tc_start(TC1, 0);
 
-    while (1) {
-        if(flag_but1){
-            tc_stop(TC0,0);
-            gfx_mono_draw_string("               ", 0, 0, &sysfont);
+	while (1) {
+		if(flag_but1){
+			tc_stop(TC0,0);
+			gfx_mono_draw_string("               ", 0, 0, &sysfont);
 			gfx_mono_draw_string("               ", 0, 16, &sysfont);
-            for(int i = 0; i< 5; i++){
-                gfx_mono_draw_pixel(10 + i*10, 32 - (int)measures[i]/12.5, GFX_PIXEL_SET);
-            }
-            flag_trig=1;
-        }
+			for(int i = 0; i< 5; i++){
+				gfx_mono_draw_pixel(10 + i*10, 32 - (int)measures[i]/12.5, GFX_PIXEL_SET);
+			}
+			flag_trig=1;
+		}
 
-        if (flag_trig) {
-        	tc_start(TC1, 0);
-            trig_pulse();
-            flag_trig = 0;
-        }
+		if (flag_trig) {
+			tc_start(TC1, 0);
+			trig_pulse();
+			flag_trig = 0;
+		}
 
-        if (flag_sensor_problems > 1){
+		if (flag_sensor_problems > 1){
 			gfx_mono_draw_string("               ", 0, 0, &sysfont);
 			gfx_mono_draw_string("Sensor problems", 0, 0, &sysfont);
-        }
+		}
 		
-        if (flag_echo_rise) {
-            RTT_init(FREQ, 0, 0);
-            flag_echo_rise = 0;
-            flag_sensor_problems = 0;
-        }
+		if (flag_echo_rise) {
+			RTT_init(FREQ, 0, 0);
+			flag_echo_rise = 0;
+			flag_sensor_problems = 0;
+		}
 
-        if (flag_echo_fall){
-            double measure = rtt_read_timer_value(RTT)*0.000058*34000;
-            if (flag_add_measure){
-                measures[flag_idx_measures] = measure;
-            }
+		if (flag_echo_fall){
+			double measure = rtt_read_timer_value(RTT)*0.000058*34000;
+			if (flag_add_measure){
+				measures[flag_idx_measures] = measure;
+			}
 			gfx_mono_draw_string("             ", 0, 0, &sysfont);
-            sprintf(str, "%.2lf cm",measure);
-            gfx_mono_draw_string(str, 0, 0, &sysfont);
+			sprintf(str, "%.2lf cm",measure);
+			gfx_mono_draw_string(str, 0, 0, &sysfont);
 			flag_echo_fall = 0;
-            flag_add_measure = 0;
-        }
-    }
+			flag_add_measure = 0;
+		}
+	}
 }
